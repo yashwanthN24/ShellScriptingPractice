aadcddd2af5d5cfbf7817c83dbc9aca66ce33308
YashwanthN24 <106897859+yashwanthN24@users.noreply.github.com>
2026-04-18 01:08:40 +0530

    at automation

---
+++ BashScripts/45_automation_at.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+rm redirect.log
+echo "Hello world" > redirect.log 
+
+
+# In terminal type 
+# at 1:05 PM 
+# > bash script.sh 
+# Ctrl + D 
+
+# check date and atq once its that time its gets excuted once 
